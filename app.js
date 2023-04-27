9b7be9058595457319c6f5f0561bb950d9007e3f
sratanota <89356791+sratanota@users.noreply.github.com>
2023-04-28 02:07:31 +0700

    add login and my courses

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 4000;
+const jwt = require('jsonwebtoken')
+
+const SECRET = "mysecretword"
 
 const pgp = require('pg-promise')(/* options */)
 const db = pgp('postgres://db_486example_user:9N2KSOdKB4W8CADTodmTWPjhp2Ks7Riw@dpg-cggkfk02qv28tc48fmk0-a/db_486example')
@@ -15,6 +18,8 @@ const courseList = [{ code: "DT160", cname: "C programming", description: "Dummy
 { code: "DT163", cname: "OOP programming Lab", description: "Dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever sin" },
 { code: "DT468", cname: "Special Topics", description: "Dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever sin" },
 { code: "DT261", cname: "Data Structures", description: "Dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever sin" }]
+
+const userData = {email:"mail1@xxx.com", password:"pass1"}
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -70,6 +75,56 @@ app.get('/courseList', (req, res) => {
       res.send("ERROR: can't get data")
     })
 
+})
+
+const generateToken = (usr)=> {
+  const token = jwt.sign({email:usr.email} , SECRET, {expiresIn:"30m",algorithm:"HS256"})
+  return token;
+}
+app.post('/login',(req, res) => {
+  const {user} = req.body
+
+  db.any('select email, password from public.student where email = $1 and password = $2',user.email, user.password)
+  .then((data) => {
+    if (data.length === 0){
+      return res.status(401).send("Login Fail.")
+    }
+    const access_token = generateToken(user)
+    res.json({token:access_token})
+  })
+  .catch((error) => {
+    return res.status(401).send("Login Fail.")
+  })
+})
+
+const validateToken = (req, res, next)=>{
+  if (!req.headers["authorization"]) return res.status(401).send("Not Login")
+  const token = req.headers["authorization"].replace("Bearer ","")
+  jwt.verify(token,SECRET, (err, email)=>{
+    if(err){
+      return res.status(401).send("Not Login")
+    }else{
+      req.email = email
+      next()
+    }
+  })
+}
+app.post('myCourses', validateToken ,(req,res) =>{
+  const {email} = req.body
+  console.log("email",email)
+  const queryString = `select  co.coursecode, course.description, sem.semester, sem.year  from studentenrollment as se join student s on se.studentid = s.id 
+  join courseopenning as co on se.copenid = co.cid
+  left join course on co.coursecode = course.code 
+  left join semester as sem on co.semid = sem.semid
+  where s.email = $1`
+  db.any(queryString,email)
+    .then((data) => {
+      res.json({result:data})
+    })
+    .catch((error) => {
+      console.log('ERROR:', error)
+      res.send("ERROR: can't get data")
+    })  
 })
 
 app.get('/students', (req, res) => {
